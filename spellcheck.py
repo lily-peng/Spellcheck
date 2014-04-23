ea1339028ff00e8ebaedfe132661a07ed3706c51
lpeng32 <mintylilac@yahoo.com>
2014-04-22 21:50:11 -0400

    replaced file
    changed from trie implementation to regex

--- spellcheck.py
+++ spellcheck.py
@@ -1,82 +1,95 @@
 # Lily Peng
-# April 5, 2014
+# April 9, 2014
 # Code for Twitch.tv's spellcheck problem: http://www.twitch.tv/problems/spellcheck
 # This module will read a misspelled word from the user,
 # then return the closest word from a locally stored .txt file
-# using a trie implementation.
 
-vowels = ["a", "e", "i", "o", "u"]
+import re  # Regex
+from string import ascii_lowercase
 
-def my_trie(words):
-    """ My trie implementation """
-    root = dict()
-    for word in words:
-        current_dict = root
-        for raw_letter in word:
-            letter = raw_letter.lower()
-            current_dict = current_dict.setdefault(letter, {})
-            current_dict["_self"] = letter
-        current_dict = current_dict.setdefault("_end", words.index(word))
-    return root
 
 def is_vowel(letter):
     """ Checks if the inputted letter is a vowel. """
-    if letter in vowels:
+    if letter in ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U"):
         return True
     return False
 
-def iterate(curr, word, index, results):
-    """ Performs the main search through the trie. """
-    to_visit = []
-    if index < len(word):
-        letter = word[index]
-        for key in curr.keys():  # For every child letter of curr
-            exact_match = (key == letter)
-            vowel_match = is_vowel(key) and is_vowel(letter)
-            match = exact_match or vowel_match 
-            if match:
-                next_child = curr[key]
-                if isinstance(next_child, dict):
-                    for key2 in next_child.keys():  # For every child letter of next_child
-                        if key2 == "_end":  # Reached the end of a word
-                            if index == len(word)-1:
-                                results.append(next_child[key2])
-                to_visit.append(curr[key])  # Adding letters
-				
-        temp = curr.get("_self","")  # Previous letter, or curr letter
-        vowel_matches = is_vowel(temp) and is_vowel(letter)
-        if is_vowel(temp):
-            for vowel in vowels:
-                if not curr.get(vowel,"") == "":
-                    break
-        if letter == temp or (vowel_matches):
-            if index == len(word)-1:  # Repeating letter at end of word
-                results.append(curr.get("_end"))
-            to_visit.append(curr) # Requeue itself
-        while not to_visit == []:
-            node = to_visit.pop(-1)
-            iterate(node, word, index+1, results)
+def make_pattern(original):
+    """ This function generates a regex pattern specifically for this spellcheck from the original inputted string. """
+    original = original.lower()
+    pattern = ""
+    prev = ""
+    count = 1  # How many times a char appears in a row
+    for index in range(len(original)+1):
+        if index == len(original):  # Reached the empty after the string
+            letter = ""
+        else:
+            letter = original[index:index+1]
+        if letter == prev or (is_vowel(letter) and is_vowel(prev)):
+            if count == 1:
+                if is_vowel(letter):
+                    pattern = pattern[:-7] + "(" + pattern[-7:]  # Insert parens before [aeiou]
+            count = count + 1
+        elif count > 1:
+            if is_vowel(prev):
+                pattern = pattern + ")"
+            pattern = pattern + "{1," + str(count) + "}"
+            count = 1
+        if is_vowel(letter) and count == 1:
+            pattern = pattern + "[aeiou]"
+        elif count == 1:
+            pattern = pattern + letter
+        prev = letter
 
-def search_results(root, word):
-    """ Returns results from the trie search. """
-    results = []
-    iterate(root, word, 0, results)
-    if len(results) == 0:
-        return "NO SUGGESTIONS"
+    return pattern
+
+
+file_list = [(line.rstrip()) for line in open("words.txt")]  # Change this file to your dict file directory!
+alphabet = ascii_lowercase
+vowels = "eiou"
+for i in range(len(vowels)):
+    alphabet = alphabet.replace(vowels[i],"")
+
+alphabet = list(alphabet)
+lookup = [[]] * 22  # List of words that start with each letter
+for word in file_list:
+    if is_vowel(word[0]):
+        if len(lookup[0]) == 0:
+            lookup[0] = [word]
+        else:
+            lookup[0].append(word) 
     else:
-        for r in results:
-            if word == file_list[r].lower():
-                return file_list[r]
-        return file_list[(results[0])]
-
-print("Loading...")
-file_list = [(line.rstrip()) for line in open('words.txt')]
-trie = my_trie(file_list)
-to_visit = []
+        i = alphabet.index(word[0].lower())
+        if len(lookup[i]) == 0:
+            lookup[i] = [word]
+        else:
+            lookup[i].append(word)
 
 while True:
-    word = input(">")
-    word = word.lower()
-    index = 0
-    curr = trie
-    print(search_results(curr, word))
+    word_input = input(">")
+    if word_input == "":
+        print("NO SUGGESTION")
+    else:
+        pattern = make_pattern(word_input)
+        word = word_input.lower()
+        curr = []
+        first_letter = word[0]
+        if first_letter.isalpha():
+            if is_vowel(first_letter):
+                curr.extend(lookup[0])
+            else:
+                curr = lookup[alphabet.index(first_letter)]
+
+        flag = False
+        if curr == []:
+            print("NO SUGGESTION")
+            flag = True
+        else:
+            for result in curr:
+                match = re.fullmatch(pattern, result, re.I)
+                if match:
+                    flag = True
+                    print(result)
+                    break
+        if flag == False:
+            print("NO SUGGESTION")
